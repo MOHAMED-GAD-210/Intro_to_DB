8e23561367463c6eda90047496363d1493d9f4f8
MOHAMED-GAD-210 <arabianights200001@gmail.com>
2026-03-04 11:37:12 -0800

    8th week

--- task_4.sql
+++ task_4.sql
@@ -1,15 +1,5 @@
 -- task_4.sql
--- Print full description of table "books" without using DESCRIBE or EXPLAIN
--- Database name will be passed as argument to mysql command
-
-SELECT 
-    COLUMN_NAME AS 'Column Name',
-    COLUMN_TYPE AS 'Data Type',
-    IS_NULLABLE AS 'Nullable',
-    COLUMN_KEY AS 'Key',
-    COLUMN_DEFAULT AS 'Default',
-    EXTRA AS 'Extra'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()      -- يستخدم قاعدة البيانات الحالية
-  AND TABLE_NAME = 'books'
-ORDER BY ORDINAL_POSITION;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE()
+AND TABLE_NAME = 'books';
